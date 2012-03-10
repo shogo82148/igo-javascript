7118a8d5cce054ad1bfd351a69a0096421793c94
shogo82148 <shogo82148@gmail.com>
2012-03-11 00:15:46 +0900

    First Commit

---
+++ dictionary.js
@@ -0,0 +1,77 @@
+
+//Viterbiアルゴリズムで使用されるノード
+igo.ViterbiNode = function(wordId, start, length, cost, leftId, rightId, isSpace) {
+	this.cost = cost; //始点からノードまでの総コスト
+	this.prev = undefined; //コスト最小の前方のノードへのリンク
+	this.wordId = wordId; //単語ID
+	this.start = start; //入力テキスト内での形態素の開始位置
+	this.length = length; //形態素の表層形の長さ(文字数)
+	this.leftId = leftId; //左文脈ID
+	this.rightId = rightId; //右文脈ID
+	this.isSpace = isSpace; //形態素の文字種(文字カテゴリ)が空白文字かどうか
+};
+
+igo.ViterbiNode.makeBOSEOS = function() {
+	return new igo.ViterbiNode(0, 0, 0, 0, 0, 0, false);
+};
+
+//形態素の連接コスト表を扱うクラス
+igo.Matrix = function(buffer, bigendian) {
+	fmis = new igo.ArrayBufferStream(buffer, bigendian);
+	this.leftSize = fmis.getInt();
+	this.rightSize = fmis.getInt();
+	this.matrix = fmis.getShortArray(this.leftSize * this.rightSize);
+};
+
+//形態素同士の連接コストを求める
+igo.Matrix.prototype.linkCost = function(leftId, rightId) {
+	return this.matrix.get(rightId * this.leftSize + leftId);
+};
+
+igo.WordDic = function(word2id, worddat, wordary, wordinf, bigendian) {
+	this.trie = new igo.Searcher(word2id, bigendian);
+	this.data = igo.getCharArray(worddat, bigendian);
+	this.indices = igo.getIntArray(wordary, bigendian);
+	
+	var fmis = new igo.ArrayBufferStream(wordinf, bigendian);
+	var wordCount = fmis.size() / (4 + 2 + 2 + 2);
+	
+	//dataOffsets[単語ID] = 単語の素性データの開始位置
+	this.dataOffsets = fmis.getIntArray(wordCount);
+	
+	//leftIds[単語ID] = 単語の左文脈ID
+	this.leftIds = fmis.getShortArray(wordCount);
+	
+	//rightIds[単語ID] = 単語の右文脈ID
+	this.rightIds = fmis.getShortArray(wordCount);
+	
+	//consts[単語ID] = 単語のコスト
+	this.costs = fmis.getCharArray(wordCount);
+};
+
+igo.WordDic.prototype = {
+	search: function(text, start, callback) {
+		var costs = this.costs;
+		var leftIds = this.leftIds;
+		var rightIds = this.rightIds;
+		var indices = this.indices;
+		
+		function fn(start, offset, trieId) {
+			var end = indices.get(trieId + 1);
+			for(var i=indices.get(trieId); i<end; i++) {
+				callback(new igo.ViterbiNode(i, start, offset, costs.get(i),
+						leftIds.get(i), rightIds.get(i), false));
+			}
+		}
+		this.trie.eachCommonPrefix(text, start, fn);
+	},
+	wordData: function(wordId) {
+		var res = Array();
+		var start = this.dataOffsets.get(wordId);
+		var end = this.dataOffsets.get(wordId+1);
+		for(var i=start;i<end;i++) {
+			res.push(String.fromCharCode(this.data.get(i)));
+		}
+		return res;
+	},
+};
---
+++ tagger.js
@@ -0,0 +1,115 @@
+
+//形態素クラス
+igo.Morpheme = function(surface, feature, start) {
+	this.surface = surface; //形態素の表層形
+	this.feature = feature; //形態素の素性
+	this.start = start; //テキスト内での形態素の出現開始位置
+};
+
+
+igo.Tagger = function(wdc, unk, mtx) {
+	this.wdc = wdc;
+	this.unk = unk;
+	this.mtx = mtx;
+};
+
+igo.Tagger.prototype = {
+	parse: function(text) {
+		var result = [];
+		var vn = this.parseImpl(text);
+		while(vn) {
+			var surface = text.substring(vn.start, vn.start + vn.length);
+			var feature = this.wdc.wordData(vn.wordId).join('');
+			result.push(new igo.Morpheme(surface, feature, vn.start));
+			vn = vn.prev;
+			console.log(surface, feature);
+		}
+		return result;
+	},
+	
+	parseImpl: function(text) {
+		var length = text.length;
+		var nodesAry = new Array(length + 1);
+		nodesAry[0] = [igo.ViterbiNode.makeBOSEOS()];
+		
+		var wdc = this.wdc
+		var unk = this.unk
+		var tagger = this;
+		var ml = new igo.MakeLattice(nodesAry, function(vn, prevs) {
+			return tagger.setMincostNode(vn, prevs);
+		});
+		var fn = function(vn) {
+			ml.call(vn);
+		};
+		for(var i=0; i<length; i++) {
+			if(!nodesAry[i]) continue;
+			ml.set(i);
+			wdc.search(text, i, fn) //単語辞書から形態素を検索
+		}
+		
+		var cur = this.setMincostNode(igo.ViterbiNode.makeBOSEOS(),
+				nodesAry[length]).prev;
+		
+		var head = undefined;
+		while(cur.prev) {
+			var tmp = cur.prev;
+			cur.prev = head;
+			head = cur;
+			cur = tmp;
+		}
+		return head;
+	},
+	
+	setMincostNode: function(vn, prevs) {
+		var mtx = this.mtx
+		var leftId = vn.leftId
+		var f = prevs[0];
+		vn.prev = prevs[0];
+		var minCost = f.cost + mtx.linkCost(f.rightId, leftId);
+		
+		for(var i=1; i<prevs.length; i++) {
+			var p = prevs[i];
+			var cost = p.cost + mtx.linkCost(p.rightId, leftId)
+			if(cost < minCost) {
+				minCost = cost;
+				vn.prev = p
+			}
+		}
+		vn.cost += minCost
+		return vn;
+	},
+};
+
+igo.MakeLattice = function(nodesAry, setMincostNode) {
+	this.nodesAry = nodesAry;
+	this.i = 0;
+	this.prevs = undefined;
+	this.empty = true;
+	this.setMincostNode = setMincostNode;
+};
+
+igo.MakeLattice.prototype = {
+	set: function(i) {
+		this.i = i;
+		this.prevs = this.nodesAry[i];
+		this.nodesAry[i] = undefined;
+		this.empty = true;
+	},
+	
+	call: function(vn) {
+		this.empty = false;
+		var nodesAry = this.nodesAry;
+		var end = this.i + vn.length
+		var ends = nodesAry[end] || [];
+		if(vn.isSpace) {
+			ends = ends.concat(this.prevs);
+		} else {
+			ends.push(this.setMincostNode(vn, this.prevs));
+		}
+		nodesAry[end] = ends;
+	},
+	
+	isEmpty: function() {
+		return this.empty;
+	},
+};
