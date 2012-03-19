46ca526cf87e3512851a3b02be340b9b3afa537d
Ichinose Shogo <shogo82148@gmail.com>
2012-03-20 00:23:04 +0900

    file system test

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var fs = require('fs');
  
 var app = express.createServer();
- 
+
 app.get('/', function(req, res) {
-	res.send('Hello World');
-});
+	    var text = fs.readFileSync('./web.js', 'utf-8');
+	    res.send('Hello World' + text);
+	});
  
 var port = process.env.PORT || 3000;
 app.listen(port);
