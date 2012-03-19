baad3072ad30d1993965627216a832f164637be3
Ichinose Shogo <shogo82148@gmail.com>
2012-03-19 23:24:10 +0900

    Init

---
+++ web.js
@@ -0,0 +1,11 @@
+var express = require('express');
+ 
+var app = express.createServer();
+ 
+app.get('/', function(req, res) {
+	res.send('Hello World');
+});
+ 
+var port = process.env.PORT || 3000;
+app.listen(port);
+)
